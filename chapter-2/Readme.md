68a5570f9fce1ccac322c259345600c13ddf74c8
Duy Nguyễn <duy@Duys-MacBook-Air.local>
2022-06-05 21:32:08 +0700

    create asg launch template

--- chapter-2/Readme.md
+++ chapter-2/Readme.md
@@ -61,3 +61,54 @@ ingress {
               docker run -d -p ${var.nginx_port}:80 nginx:alpine
               EOF
 ```
+
+5. Create ASG
+
+get subnet ids using Terraform data source
+```hcl
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [ data.aws_vpc.default.id ]
+  }
+}
+```
+
+create launch templete
+
+```hcl
+resource "aws_launch_template" "nginx" {
+  name = "nginx"
+  image_id = "ami-0bd6906508e74f692"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [ aws_security_group.sg_test.id ]
+
+  user_data = "${base64encode(<<-EOF
+              #!/bin/bash
+              amazon-linux-extras install docker -y
+              service docker start
+              docker run -d -p ${var.nginx_port}:80 nginx:alpine
+              EOF
+  )}"
+}
+```
+
+create asg
+```hcl
+resource "aws_autoscaling_group" "nginx" {
+  vpc_zone_identifier = data.aws_subnets.default.ids
+  desired_capacity   = 2
+  max_size           = 3
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.nginx.id
+    version = "$Latest"
+  }
+}
+```
--- chapter-2/main.tf
+++ chapter-2/main.tf
@@ -13,22 +13,61 @@ variable "ssh_port" {
   type = number
 }
 
-resource "aws_instance" "test" {
-  ami = "ami-0bd6906508e74f692"
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [ data.aws_vpc.default.id ]
+  }
+}
+
+# resource "aws_instance" "test" {
+#   ami = "ami-0bd6906508e74f692"
+#   instance_type = "t2.micro"
+
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               amazon-linux-extras install docker -y
+#               service docker start
+#               docker run -d -p ${var.nginx_port}:80 nginx:alpine
+#               EOF
+
+#   tags = {
+#     "Name" = "Terraform"
+#   }
+
+#   vpc_security_group_ids = [ aws_security_group.sg_test.id ]
+# }
+
+resource "aws_launch_template" "nginx" {
+  name = "nginx"
+  image_id = "ami-0bd6906508e74f692"
   instance_type = "t2.micro"
 
-  user_data = <<-EOF
+  vpc_security_group_ids = [ aws_security_group.sg_test.id ]
+
+  user_data = "${base64encode(<<-EOF
               #!/bin/bash
               amazon-linux-extras install docker -y
               service docker start
               docker run -d -p ${var.nginx_port}:80 nginx:alpine
               EOF
+  )}"
+}
 
-  tags = {
-    "Name" = "Terraform"
+resource "aws_autoscaling_group" "nginx" {
+  vpc_zone_identifier = data.aws_subnets.default.ids
+  desired_capacity   = 2
+  max_size           = 3
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.nginx.id
+    version = "$Latest"
   }
-
-  vpc_security_group_ids = [ aws_security_group.sg_test.id ]
 }
 
 resource "aws_security_group" "sg_test" {
