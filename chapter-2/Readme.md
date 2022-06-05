cc9f6c1cb8dd8b7e5a626e4a310754c108b9aa44
Duy Nguyễn <duy@Duys-MacBook-Air.local>
2022-06-05 16:11:47 +0700

    add user data and sg

--- chapter-2/Readme.md
+++ chapter-2/Readme.md
@@ -1,4 +1,4 @@
-Create single EC2 instance:
+1. Create single EC2 instance:
 
 ```terraform
 resource "aws_instance" "test" {
@@ -8,5 +8,27 @@ resource "aws_instance" "test" {
   tags = {
     "Name" = "Terraform"
   }
+}
+```
+
+2. Create instance with user data
+
+```terraform
+resource "aws_instance" "test" {
+  ami = "ami-0bd6906508e74f692"
+  instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              amazon-linux-extras install docker -y
+              service docker start
+              docker run -d -p 3000:80 nginx:alpine
+              EOF
+
+  tags = {
+    "Name" = "Terraform"
+  }
+
+  vpc_security_group_ids = [ aws_security_group.sg_test.id ]
 }
 ```
--- chapter-2/main.tf
+++ chapter-2/main.tf
@@ -7,7 +7,42 @@ resource "aws_instance" "test" {
   ami = "ami-0bd6906508e74f692"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              amazon-linux-extras install docker -y
+              service docker start
+              docker run -d -p 3000:80 nginx:alpine
+              EOF
+
   tags = {
     "Name" = "Terraform"
+  }
+
+  vpc_security_group_ids = [ aws_security_group.sg_test.id ]
+}
+
+resource "aws_security_group" "sg_test" {
+  name = "sg_test"
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 3000
+    protocol = "tcp"
+    to_port = 3000
+  }
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
